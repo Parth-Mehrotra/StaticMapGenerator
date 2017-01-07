bf8d553e3b1ee5d3846b9d8a12d8ef6cb5eced20
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2017-01-07 04:36:13 -0500

    quick change

---
+++ StaticMaps/src/main/java/com/parth/geo/App.java
@@ -0,0 +1,13 @@
+package com.parth.geo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
